ebdff64e88256b95886d4871bf87874311866152
hasan.souliman@yolo.clinic <hasan.souliman@yolo.clinic>
2024-11-18 23:35:41 +0300

    refactoring models

--- src/app/Models/User.ts
+++ src/app/Models/User.ts
@@ -1,8 +1,17 @@
-
 export interface User {
   id: number;
   first_name: string;
   last_name: string;
   email: string;
   avatar: string;
+}
+export interface ResponseData {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+}
+export interface ResponseDataForUser {
+  data: User;
 }
--- src/app/Services/User/user.service.ts
+++ src/app/Services/User/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
+import { ResponseData, ResponseDataForUser } from '../../Models/User';
 
 @Injectable({
   providedIn: 'root',
@@ -8,21 +9,25 @@ import { Observable, of, tap } from 'rxjs';
 export class UserService {
   private cache = new Map<string, any>();
 
-
+  public baseURl = 'https://reqres.in/api/users';
   constructor(private http: HttpClient) {}
 
-  getUsers(page: number): Observable<any> {
-    const url = `https://reqres.in/api/users?page=${page}`;
+  getUsers(page: number): Observable<ResponseData> {
+    const url = this.baseURl + `?page=${page}`;
     if (this.cache.has(url)) {
       return of(this.cache.get(url));
     }
-    return this.http.get(url).pipe(tap((data) => this.cache.set(url, data)));
+    return this.http
+      .get(url)
+      .pipe(tap((data: any) => this.cache.set(url, data)));
   }
-  getUserDetail(id: number): Observable<any> {
-    const url = `https://reqres.in/api/users/${id}`;
+  getUserDetail(id: number): Observable<ResponseDataForUser> {
+    const url = this.baseURl + `/${id}`;
     if (this.cache.has(url)) {
       return of(this.cache.get(url));
     }
-    return this.http.get(url).pipe(tap((data) => this.cache.set(url, data)));
+    return this.http
+      .get(url)
+      .pipe(tap((data: any) => this.cache.set(url, data)));
   }
 }
